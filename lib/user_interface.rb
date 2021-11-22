b97dc24869e36fb451b213fe40418d16736a87f5
MattW <mattnwarnock@yahoo.co.uk>
2021-11-22 16:50:04 +0000

    Gets valid user choice

--- lib/user_interface.rb
+++ lib/user_interface.rb
@@ -1,10 +1,36 @@
 # frozen_string_literal: true
 
 class UserInterface
-  def initialize(output)
+  VAID_OPTION = /^[1-4]$/.freeze
+  VAILD_MONEY_AMOUNT = /^\d+(.\d{2})?$/.freeze
+
+  def initialize(input, output)
+    @input = input
     @output = output
   end
 
+  def menu_choice(options)
+    print_menu(options)
+    output.puts 'Choose an option: '
+    valid_input { |user_input| user_input.match?(VAID_OPTION) }
+  end
+
+  private
+
+  attr_reader :input, :output
+
   def print_menu(options)
+    options.each_index do |index|
+      output.puts "#{index + 1}. #{options[index]}"
+    end
+  end
+
+  def valid_input
+    loop do
+      user_input = input.gets.chomp
+      break user_input.to_i if yield user_input
+
+      output.puts 'Please enter a valid choice'
+    end
   end
 end
--- spec/user_interface_spec.rb
+++ spec/user_interface_spec.rb
@@ -3,20 +3,41 @@
 require 'user_interface'
 
 RSpec.describe UserInterface do
+  let(:valid_choice) { StringIO.new("1\n") }
+  let(:options) { ['Deposit', 'Withdrawal', 'Account Statment', 'Exit'] }
   let(:output) { StringIO.new }
-  let(:user_interface) { described_class.new(output) }
+  let(:user_interface) { described_class.new(valid_choice, output) }
 
-  describe '#print_menu' do
+  describe '#menu_choice' do
     it 'prints all app options' do
-      options = ['Deposit', 'Withdrawal', 'Account Statment', 'Exit']
+      user_interface.menu_choice(options)
 
-      user_interface.print_menu(options)
-
-      expect(output.string).to eq "1. #{options[0]}
+      expect(output.string).to include "1. #{options[0]}
 2. #{options[1]}
 3. #{options[2]}
 4. #{options[3]}
 "
+    end
+
+    it 'prompts user to enter an option' do
+      user_interface.menu_choice(options)
+
+      expect(output.string).to include 'Choose an option: '
+    end
+
+    it 'returns a user choice' do
+      user_choice = user_interface.menu_choice(options)
+
+      expect(user_choice).to eq 1
+    end
+
+    it 'only accepts a vaild choice' do
+      invalid_choice = StringIO.new("5\n1\n")
+      user_interface = described_class.new(invalid_choice, output)
+
+      user_interface.menu_choice(options)
+
+      expect(output.string).to include 'Please enter a valid choice'
     end
   end
 end
