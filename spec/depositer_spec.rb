e756fd627a2a7d002e7ed2e9d223d390c7de01fa
MattW <mattnwarnock@yahoo.co.uk>
2021-11-23 21:12:45 +0000

    Test for timestamp with test refactor

--- spec/depositer_spec.rb
+++ spec/depositer_spec.rb
@@ -7,24 +7,36 @@ require 'user_interface'
 RSpec.describe Depositer do
   let(:account) { AccountDatabase.new }
   let(:output) { StringIO.new }
+  let(:deposit_amount) { 1106.95 }
 
   describe '#run' do
     it 'prints prompt for amount to deposit' do
-      depositer_initialize("1106.95\n").run
+      depositer_initialize.run
 
       expect(output.string).to include UserInterface::DEPOSIT
     end
 
     it 'deposits amount into account' do
-      depositer_initialize("1106.95\n").run
+      depositer_initialize.run
 
-      expect(account.currant_balance).to eq 1106.95
+      expect(account.currant_balance).to eq deposit_amount
+    end
+
+    it 'adds currant time to the transaction' do
+      today = Time.utc(2023, 1, 10, 18, 24, 38)
+      allow(Time).to receive(:now).and_return(today)
+
+      depositer_initialize.run
+
+      transaction_time = account.all_transactions.last[:unix_time]
+
+      expect(transaction_time).to eq today.to_i
     end
   end
 
-  def depositer_initialize(input)
+  def depositer_initialize
     options = ['Deposit', 'Withdrawal', 'Account Statment', 'Exit']
-    user_interface = UserInterface.new(StringIO.new(input), output, options)
+    user_interface = UserInterface.new(StringIO.new("#{deposit_amount}\n"), output, options)
 
     Depositer.new(account, user_interface)
   end
