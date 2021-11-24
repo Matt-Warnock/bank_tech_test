f8b0884e3605145cd4f662691e9813187e78caf0
MattW <mattnwarnock@yahoo.co.uk>
2021-11-24 15:27:36 +0000

    Create new transaction for withdrawral

--- lib/withdrawer.rb
+++ lib/withdrawer.rb
@@ -9,10 +9,22 @@ class Withdrawer
   end
 
   def run
-    user_interface.prompt_amount_for(UserInterface::WITHDRAWAL).to_f
+    withdrawal = user_interface.prompt_amount_for(UserInterface::WITHDRAWAL).to_f
+
+    new_balance = account.currant_balance - withdrawal
+
+    account.add_transaction(
+      unix_time: unix_time_now,
+      debit: withdrawal,
+      balance: new_balance
+    )
   end
 
   private
 
-  attr_reader :user_interface
+  attr_reader :account, :user_interface
+
+  def unix_time_now
+    Time.now.to_i
+  end
 end
--- spec/withdrawer_spec.rb
+++ spec/withdrawer_spec.rb
@@ -12,18 +12,20 @@ RSpec.describe Withdrawer do
   let(:withdrawal_amount) { 35.95 }
 
   describe '#run' do
+    before(:each) { add_to_account(initial_deposit) }
+
     it 'prints prompt for amount to withdraw' do
       withdrawer_initialize.run
 
       expect(output.string).to include UserInterface::WITHDRAWAL
     end
 
-    xit 'adds transaction to account' do
+    it 'adds transaction to account' do
       expect { withdrawer_initialize.run }
         .to change { account.all_transactions.count }.from(1).to(2)
     end
 
-    xit 'takes away withdrawal amount from balance' do
+    it 'takes away withdrawal amount from balance' do
       balance_after_withdrawal = initial_deposit - withdrawal_amount
 
       expect { withdrawer_initialize.run }
@@ -31,7 +33,7 @@ RSpec.describe Withdrawer do
         .from(initial_deposit).to(balance_after_withdrawal)
     end
 
-    xit 'adds currant time to the transaction' do
+    it 'adds currant time to the transaction' do
       allow(Time).to receive(:now).and_return(today)
 
       withdrawer_initialize.run
@@ -77,7 +79,6 @@ RSpec.describe Withdrawer do
     options = ['Deposit', 'Withdrawal', 'Account Statment', 'Exit']
     user_interface = UserInterface.new(StringIO.new("#{withdrawal_amount}\n"), output, options)
 
-    add_to_account(initial_deposit)
     Withdrawer.new(account, user_interface)
   end
 
